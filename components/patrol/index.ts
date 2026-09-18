5078112aee387ab1082df1a4810de44a59efdae5
Frederick de Ruiter <127706008+fderuiter@users.noreply.github.com>
2026-09-18 03:18:23 -0500

    feat(patrol): wire playable vertical slice and mountain map hub (#749)

--- components/patrol/index.ts
+++ components/patrol/index.ts
@@ -1,2 +1,15 @@
+/**
+ * Public barrel exports for the Patrol Shift UI components.
+ */
+
 export * from "./PatrolShiftContainer";
 export * from "./Skeletons";
+export * from "./IntroScreen";
+export * from "./BriefingScreen";
+export * from "./MountainMap";
+export * from "./DispatchOverlay";
+export * from "./SceneInteractionPlaceholder";
+export * from "./OetPlaceholder";
+export * from "./HandoffScreen";
+export * from "./DebriefPlaceholder";
+export * from "./ShiftSummary";
